b5b5e6f89c25f0f426eedfbf1e2486e1016e849b
Antonio Ganci <aganci@ubuntu.(none)>
2012-04-27 17:59:18 +0200

    add microposts

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -11,6 +11,14 @@ module SessionsHelper
     user == current_user
   end
 
+  def signed_in_user
+    unless signed_in?
+      store_location
+      redirect_to signin_path, notice: "Please sign in."
+    end
+  end  
+
+
   def sign_in(user)
     cookies.permanent[:remember_token] = user.remember_token
     current_user=(user)
